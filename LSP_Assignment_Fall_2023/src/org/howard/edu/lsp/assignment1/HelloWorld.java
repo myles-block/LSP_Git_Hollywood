b78f75f8700eaeb2a4f7d3e9c57c844212b4d199
myles-block <85708958+myles-block@users.noreply.github.com>
2023-08-31 14:08:01 -0400

    Initial commit.

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/**
+		 * Myles Block
+		 */
+		System.out.println("Hello World");
+		
+	}
+
+}
