29ce6cc6bf27a16d353e16a57d58fcfef9d6ed67
myles-block <85708958+myles-block@users.noreply.github.com>
2023-10-19 19:54:24 -0400

    Updated Book & Driver

--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Book.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Book.java
@@ -1,5 +1,33 @@
 package org.howard.edu.lsp.midterm.problem1;
 
-public class Problem1 {
+public class Book {
+	private String title;
+	private String author;
+	private int year;
+	
+	public Book(String title, String author, int year) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		}
+	
+	@Override
+	public String toString() {
+		return "Title: " + title + ", Author: " + author + ", Year: " + year;
+		}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Book)) {
+			return false;
+		}
+		Book otherBook = (Book) obj;
+		return (title.equals(otherBook.title) &&
+		author.equals(otherBook.author));
+		}
+
 
 }
--- LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Driver.java
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Driver.java
@@ -1,5 +1,14 @@
 package org.howard.edu.lsp.midterm.problem1;
 
-public class Problem1Driver {
-
+public class Driver {
+    public static void main(String[] args) {
+        Book book1 = new Book("To Kill a MockingBird", "Harper Lee", 1960);
+        Book book2 = new Book("To Kill a MockingBird", "Harper Lee", 1960);
+        Book book3 = new Book("1984", "George Orwell", 1960);
+        System.out.println("book 1 equals book2: " + book1.equals(book2));
+        System.out.println("book 1 equals book3: " + book1.equals(book3));
+	   System.out.println("Display of book’s title, author and year: " + 
+       book1.toString());
+    }
 }
+
